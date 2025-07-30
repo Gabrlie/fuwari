87b51cadefebe246ccc60059f8e956718fd41ae0
Gabrlie <57052263+Gabrlie@users.noreply.github.com>
2025-07-30 06:27:20 +0000

    修改关于界面

--- src/config.ts
+++ src/config.ts
@@ -13,11 +13,11 @@ export const siteConfig: SiteConfig = {
 	lang: "zh_CN", // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko', 'es', 'th', 'vi'
 	themeColor: {
 		hue: 250, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
-		fixed: true, // Hide the theme color picker for visitors
+		fixed: false, // Hide the theme color picker for visitors
 	},
 	banner: {
-		enable: false,
-		src: "assets/images/demo-banner.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+		enable: true,
+		src: "https://t.alcy.cc/pc", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 		position: "center", // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
 		credit: {
 			enable: false, // Display the credit text of the banner image
@@ -45,7 +45,7 @@ export const navBarConfig: NavBarConfig = {
 		LinkPreset.Archive,
 		LinkPreset.About,
 		{
-			name: "GitHub",
+			name: "统计",
 			url: "https://umami.gabrlie.top/share/aYsGHqFzCOLYe7IQ/astro.gabrlie.cn", // Internal links should not include the base path, as it is automatically added
 			external: true, // Show an external link icon and will open in a new tab
 		},
@@ -53,16 +53,10 @@ export const navBarConfig: NavBarConfig = {
 };
 
 export const profileConfig: ProfileConfig = {
-	avatar:
-		"https://dn-qiniu-avatar.qbox.me/avatar/9afd256fc0f0acc48512baeb247cfce2", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+	avatar: "https://avatars.githubusercontent.com/u/57052263?v=4", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 	name: "Gabrlie",
 	bio: "Code create the world.",
 	links: [
-		{
-			name: "Bilibili",
-			icon: "fa6-brands:bilibili",
-			url: "https://space.bilibili.com/62969091",
-		},
 		{
 			name: "GitHub",
 			icon: "fa6-brands:github",
